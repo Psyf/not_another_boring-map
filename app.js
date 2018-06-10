13b4bb23936b09169868e4021757bedaf702016b
SaifUM <gzsaif001@gmail.com>
2018-06-10 23:12:35 +0600

    disabled mongoDB for heroku deploy

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 //importing required modules
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes/api');   
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes/api");
+const bodyParser = require("body-parser");
 
 //initializing app
-const app = express(); 
+const app = express();
 
 //Connecting to MongoDB
-mongoose.connect('mongodb://localhost/map');
-mongoose.Promise = global.Promise; 	//not sure what this does 
+//mongoose.connect("mongodb://localhost/map");
+//mongoose.Promise = global.Promise; //not sure what this does
+
+app.set("view engine", "ejs");
 
 //middleware optimization for static files
-app.use(express.static('public')); 
+app.use(express.static("public"));
 
 //middleware for bodyParsing with json
-app.use(bodyParser.json());  
+app.use(bodyParser.json());
 
 //setting up routes
-app.use('/', routes); 
+app.use("/", routes);
 
 //Start listening
-app.listen(process.env.port || 4000, function(){
-	console.log("listening to clients"); 
-}); 
+app.listen(process.env.port || 4000, function() {
+	console.log("listening to clients");
+});
