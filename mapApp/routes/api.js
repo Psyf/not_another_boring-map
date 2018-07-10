4c3f3616e13c213177837881439f7f426bb345df
SaifUM <gzsaif001@gmail.com>
2018-07-10 21:32:35 +0600

    Password hash (bcryptjs) stored+used instead of plain

--- mapApp/routes/api.js
+++ mapApp/routes/api.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 const Act = require("../models/activity");
 const ejs = require("ejs");
-const path = require("path"); 
+const path = require("path");
+const bcrypt = require("bcryptjs");
 
-router.get("/", function(req, res) { 
-	Act.find({category: "public", deleted: false}, function(err, acts) {
-		res.render(path.resolve(__dirname + "/../views/home.ejs"), {activities: acts}); 
-	}); 
+router.get("/", function(req, res) {
+	Act.find({ category: "public", deleted: false }, function(err, acts) {
+		res.render(path.resolve(__dirname + "/../views/home.ejs"), {
+			activities: acts
+		});
+	});
 });
 
 //TO DO: BLOCK THIS OFF
@@ -18,82 +21,111 @@ router.get("/activity", function(req, res) {
 });
 
 router.get("/activity/:id/chat", function(req, res) {
-	res.redirect('http://localhost:5000/'+ req.params.id); 
+	res.redirect("http://localhost:5000/" + req.params.id);
 });
 
 //to CREATE an activity-------------------------------------------------------------------------
 router.get("/activity/new", function(req, res) {
-	res.sendFile(path.resolve(__dirname + "/../views/new_activity.html")); 
+	res.sendFile(path.resolve(__dirname + "/../views/new_activity.html"));
 });
 
 router.post("/activity/new", function(req, res) {
-	var formData = JSON.parse(JSON.stringify(req.body)); 
-	
-	var entry = {
-		activityName: formData.activityName, 
-		category: formData.category, 
-		organizer: formData.orgName, 
-		contact: formData.contact, 
-		email: formData.email, 
-		time: [formData.startTime, formData.endTime], 
-		location: [formData.lat, formData.lng], 
-		description: formData.description, 
-		password: formData.password, 
-		deleted: false
-	}
-
-	Act.create(entry).then(function(act) {
-		res.status(200).send("You will need this password to edit/delete the activity later: " + formData.password);
+	var formData = JSON.parse(JSON.stringify(req.body));
+	bcrypt.genSalt(10, function(err, salt) {
+		bcrypt.hash(req.body.password, salt, function(err, hash) {
+			let entry = {
+				activityName: formData.activityName,
+				category: formData.category,
+				organizer: formData.orgName,
+				contact: formData.contact,
+				email: formData.email,
+				time: [formData.startTime, formData.endTime],
+				location: [formData.lat, formData.lng],
+				description: formData.description,
+				password: hash,
+				deleted: false
+			};
+			Act.create(entry).then(function(act) {
+				res.status(200).send(
+					"You will need this password to edit/delete the activity later: " +
+						formData.password
+				);
+				//res.redirect('http://localhost:4000'); 	//TO DO : redirect to activity page
+			});
+		});
 	});
-	//res.redirect('http://localhost:4000'); 	//TO DO : redirect to activity page  
-}); 
+});
 //-------------------------------------------------CREATE code enbds here------------------------------------
 
 //to EDIT an activity---------------------------------------------------------------------------------------
 router.get("/activity/edit/:id", function(req, res) {
-	res.sendFile(path.resolve(__dirname + "/../views/password_req.html")); 
+	res.sendFile(path.resolve(__dirname + "/../views/password_req.html"));
 });
 
 router.get("/activity/edit/:id/:password", function(req, res) {
 	Act.findById(req.params.id)
-	.then(function(act) {
-		if (act.password == req.params.password) {
-			res.render(path.resolve(__dirname + "/../views/edit_activity.ejs"), {activity: act});
-		}
-		else {
-			res.sendFile(path.resolve(__dirname + "/../views/password_fail.html"));
-		}
-	}).catch(err => console.log(err)); 
+		.then(function(act) {
+			bcrypt
+				.compare(req.params.password, act.password)
+				.then(function(match) {
+					if (match === true) {
+						res.render(
+							path.resolve(
+								__dirname + "/../views/edit_activity.ejs"
+							),
+							{ activity: act }
+						);
+					} else {
+						res.sendFile(
+							path.resolve(
+								__dirname + "/../views/password_fail.html"
+							)
+						);
+					}
+				});
+		})
+		.catch(err => console.log(err));
 });
 
 router.post("/activity/edit/:id/:password", function(req, res) {
-	Act.findById({_id: req.params.id}).then(
-		function(act) {
-			if (act.password == req.params.password) {
-				var formData = JSON.parse(JSON.stringify(req.body)); 
-				var entry = {
-					activityName: formData.activityName, 
-					category: formData.category, 
-					organizer: formData.orgName, 
-					contact: formData.contact, 
-					email: formData.email, 
-					time: [formData.startTime, formData.endTime], 
-					location: [formData.lat, formData.lng], 
-					description: formData.description, 
-					password: formData.password, 
-					deleted: false
-				}
-
-				Act.findByIdAndUpdate({_id: req.params.id}, entry, {new: true}, function(err, act) {
-					res.send("Successfully editted activity!"); 
+	Act.findById({ _id: req.params.id }).then(function(act) {
+		bcrypt.compare(req.params.password, act.password).then(function(match) {
+			if (match === true) {
+				var formData = JSON.parse(JSON.stringify(req.body));
+				bcrypt.genSalt(10, function(err, salt) {
+					bcrypt.hash(req.body.password, salt, function(err, hash) {
+						let entry = {
+							activityName: formData.activityName,
+							category: formData.category,
+							organizer: formData.orgName,
+							contact: formData.contact,
+							email: formData.email,
+							time: [formData.startTime, formData.endTime],
+							location: [formData.lat, formData.lng],
+							description: formData.description,
+							password: hash,
+							deleted: false
+						};
+					});
 				});
-			} 
-			else {
-				res.sendFile(path.resolve(__dirname + "/../views/password_fail.html"));
+
+				Act.findByIdAndUpdate(
+					{ _id: req.params.id },
+					entry,
+					{ new: true },
+					function(err, act) {
+						res.send("Successfully editted activity!");
+					}
+				);
+			} else {
+				res.sendFile(
+					path.resolve(__dirname + "/../views/password_fail.html")
+				);
 			}
-		}
-	);
+		});
+	});
 });
+
 //-----------------------------------------------------------Edit code ends here----------------------------
 
 //to DELETE an activity-----------------------------------------------------------------------------------
@@ -102,33 +134,37 @@ router.get("/activity/delete/:id", function(req, res) {
 });
 
 router.get("/activity/delete/:id/:password", function(req, res) {
-	Act.findById({_id: req.params.id}).then(function(act) {
-		if (act.password == req.params.password) 
-		{
-			Act.findByIdAndUpdate(req.params.id, {deleted: true}, function(err, act) {
+	Act.findById({ _id: req.params.id }).then(function(act) {
+		if (act.password == req.params.password) {
+			Act.findByIdAndUpdate(req.params.id, { deleted: true }, function(
+				err,
+				act
+			) {
 				if (!err) {
-					res.sendFile(path.resolve(__dirname + "/../views/delete_success.html"));
+					res.sendFile(
+						path.resolve(
+							__dirname + "/../views/delete_success.html"
+						)
+					);
+				} else {
+					res.send(err);
 				}
-				else {
-					res.send(err); 
-				}
-			}); 
-		}
-		else 
-		{
-			res.sendFile(path.resolve(__dirname + "/../views/password_fail.html"));
+			});
+		} else {
+			res.sendFile(
+				path.resolve(__dirname + "/../views/password_fail.html")
+			);
 		}
 	});
-}); 
+});
 //-------------------------------------------delete code ends here---------------------------------------
 
 //To SEARCH for an activity------------------------------------------------------------------------------
-router.post("/activity/search", function(req, res){
+router.post("/activity/search", function(req, res) {
 	var queryString = JSON.parse(JSON.stringify(req.body.query));
-	Act.find({$text: {$search: queryString}})
-       .exec(function(err, docs) {
-       		res.send(docs);  
-       });
+	Act.find({ $text: { $search: queryString } }).exec(function(err, docs) {
+		res.send(docs);
+	});
 });
 //-------------------------------------------------------------------SEARCH code ends here---------------
 
